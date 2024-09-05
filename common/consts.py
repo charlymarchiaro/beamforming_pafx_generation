f5f3c5d0e8260ec7d5bc03e6ed0644c21c723fbd
Charly Marchiaro <carlosmarchiaro@gmail.com>
2024-09-05 17:31:46 -0300

    Update consts.py

--- common/consts.py
+++ common/consts.py
@@ -4,6 +4,7 @@ PATTERN_FILE_FORMAT__MSI = 'msi'
 # Pattern types
 PATTERN_TYPE__BROADCAST = 'broadcast'
 PATTERN_TYPE__BEAMFORMING_ELEMENT = 'beamforming_element'
+PATTERN_TYPE__BEAMSWITCHING_SERVICE = 'beamswitching_service'
 
 # Comment fingerprint
 COMMENT_FINGERPRINT = '[beamforming_pafx_generation_script - Telecom Argentina]'
